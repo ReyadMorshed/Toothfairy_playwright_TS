2020317e6663355c30e15addc2d414abcaa7be01
ReyadMorshed <reyadmorshed76@gmail.com>
2025-05-20 15:01:45 +0600

    Initial commit

---
+++ config/cucumber.js
@@ -0,0 +1,50 @@
+module.exports = {
+    default: {
+        tags: process.env.npm_config_TAGS || "",
+        formatOptions: {
+            snippetInterface: "async-await"
+        },
+        paths: [
+            "src/test/features/"
+        ],
+        publishQuiet: true,
+        dryRun: false,
+        require: [
+            "src/test/steps/*.ts",
+            "src/hooks/hooks.ts"
+        ],
+        requireModule: [
+            "ts-node/register"
+        ],
+        format: [
+            "progress-bar",
+            "html:test-results/cucumber-report.html",
+            "json:test-results/cucumber-report.json",
+            "rerun:@rerun.txt"
+        ],
+        parallel: 1,
+        retry: 2 // Add this line
+    },
+    rerun: {
+        formatOptions: {
+            snippetInterface: "async-await"
+        },
+        publishQuiet: true,
+        dryRun: false,
+        require: [
+            "src/test/steps/*.ts",
+            "src/hooks/hooks.ts"
+        ],
+        requireModule: [
+            "ts-node/register"
+        ],
+        format: [
+            "progress-bar",
+            "html:test-results/cucumber-report.html",
+            "json:test-results/cucumber-report.json",
+            "rerun:@rerun.txt"
+        ],
+        parallel: 2,
+        retry: 2 // Add this line
+    }
+}
